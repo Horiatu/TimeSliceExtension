06f104764424e9e3ab68af42e6ed4eaee7d6fbab
Horia Tudosie <htudosie@esri.ca>
2015-11-25 12:38:32 -0500

    less than and more then in legend

--- TimeChartWidget.js
+++ TimeChartWidget.js
@@ -59,18 +59,18 @@ define([
       chart = new dojox.charting.Chart2D("reportChartDiv");
       chart.addPlot("default", {
           type: "Pie",
-          radius: 110,
-          labelOffset: 0,
+          //radius: 90,
+          labelOffset: 30,
           shadow:false,
-          stroke:"white",
-          labelWiring: "blue",
-          labelStyle: "columns"
+          stroke:"aliceblue",
+          //labelWiring: "darkgray",
+          //labelStyle: "columns"
       }).setTheme(dojox.charting.themes.CubanShirts);
 
       var a1 = new dojox.charting.action2d.Tooltip(chart, "default");
       var a = new dojox.charting.action2d.MoveSlice(chart, "default", {
-          duration: 200,
-          scale: 1.1,
+          duration: 500,
+          //scale: 1.1,
           shift: 10
       });
 
@@ -122,10 +122,17 @@ define([
         query.returnGeometry = false;
 
         var prevDates = {};
-        for(var i=1; i<=8; i++) {
-            var j=i*10;
-            prevDates[j]={ date: new Date().addDays(-j), count:0, features:[]};
+        var maxDays = 50;
+        var period = 10;
+
+        for(var i=1; i<=maxDays / period; i++) {
+            var j=i*period;
+            var k = i==1 
+            ? ("less than "+j+" days") 
+            : (((i-1) * period)+1 + " to " + (i*period) + " days");
+            prevDates[k]={ date: new Date().addDays(-j), count:0, features:[]};
         }
+        prevDates["more than "+(maxDays+1)+" days"]={ date: new Date().addDays(-10000), count:0, last:true, features:[]};
         
         var getSumCounts = function(dfr, dataSources) {
           if(dataSources && dataSources.length > 0) {
@@ -138,7 +145,7 @@ define([
                     var BreakException = {};
                     try {
                         for(var k in prevDates) {
-                            if(CreationDate > prevDates[k].date) {
+                            if(prevDates[k].last || CreationDate > prevDates[k].date) {
                                 prevDates[k].features.push(f.attributes.objectid);
                                 prevDates[k].count++;
                                 throw BreakException;
@@ -177,7 +184,6 @@ define([
 
           var countList = this.document.getElementById('countList');
           countList.innerHTML = '';
-          var last = "0";
 
           var dsnArr=[];
           
@@ -188,78 +194,47 @@ define([
             total += prevDates[cnt].count;
           }
 
-          for(var cnt in prevDates) {
-            var countsStr = ((last=="0") ? " less than " : (" "+last + " to ")) + cnt + " days ";
-            
-            last = cnt;
+          var liTotal = this.document.createElement('li');
+          liTotal.className = 'legend title';
+          liTotal.appendChild(document.createTextNode('Selected Incidents: '+total));
+          countList.appendChild(liTotal);
 
-            if(prevDates[cnt].count>0){
+          for(var key in prevDates) {
+            var countsStr = key;
+            
+            if(prevDates[key].count>0){
               var li = this.document.createElement('li');
               li.className = 'legend';
               var m = document.createElement('div');
               m.style['background-color']=colors[c];
 
               li.appendChild(m);
-              li.appendChild(document.createTextNode(countsStr+"  -  "+prevDates[cnt].count));
+              li.appendChild(document.createTextNode(countsStr+"  -  "));
+
+              var countNode = document.createElement('div');
+              countNode.className = 'title';
+              countNode.appendChild(document.createTextNode(prevDates[key].count));
+              li.appendChild(countNode);
               countList.appendChild(li);
 
-              dsnArr.push( {
-                y: prevDates[cnt].count,
-                text: (prevDates[cnt].count/total*100).toFixed(1)+"%",
+              dsnArr.push({
+                y: prevDates[key].count,
+                text: (prevDates[key].count/total*100).toFixed(1)+"%",
                 tooltip: countsStr +" : "+prevDates[cnt].count,
                 fontSize: 14,
                 fontColor: 'black',
                 color: colors[c]
-                });
+              });
               c = (c+1) % colors.length;
             }
           };
-          chart.addSeries("DSN", dsnArr);
+          chart.addSeries("Incidents", dsnArr);
           chart.render();
       })
     },
 
     dataSourceExpired: function (dataSource, dataSourceConfig) {
-      //alert(0);
-      //console.log(dataSource.name);
-      
       this.getDataSourceProxies().then( this.getContsByDates );
-      // // Execute the query. A request will be sent to the server to query for the features.
-      // // The results are in the featureSet
-      // this.DataSources.forEach(function(ds) {
-      //   console.log(ds.name)
-      //   //dataSource
-      //   ds.executeQuery(this.query).then(lang.hitch(this, function (featureSet) {
-
-      //     // Show the name of the data source and the number of features returned from the query
-      //     this.updateDataSourceInfoLabel(ds.name, featureSet);
-
-      //     //// Show the features in the table
-      //     //this.updateAttributeTable(featureSet, ds);
-      //   }))
-      // });
     },
-
-    // updateDataSourceInfoLabel: function (dataSourceName, featureSet) {
-
-    //   // Compose the correct string to display
-    //   var dataSourceInfo = dataSourceName;
-    //   var featureCount = featureSet.features.length;
-    //   if (featureCount === 0)
-    //     dataSourceInfo += " has no feature";
-    //   else
-    //     dataSourceInfo += " has " + featureCount + " features";
-    //   console.log(dataSourceInfo);
-
-    //   this.infoLabel.innerHTML = dataSourceInfo;
-    // },
-
-    // updateAttributeTable: function (featureSet, dataSource) {
-    //   // For each feature put them in the store and overwrite any existing
-    //   // Potential improvement: Remove from the store the features that were not part of this update.
-    //   featureSet.features.forEach(lang.hitch(this, function (feature) {
-    //     this.store.put(feature.attributes, {overwrite: true, id: feature.attributes[dataSource.objectIdFieldName]});
-    //   }));
-    // }
   });
 });
