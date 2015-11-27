728a4ccb483f0949677b81958754e0bd2b7d36b9
Horia Tudosie <htudosie@esri.ca>
2015-11-26 22:41:26 -0500

    fixed logo loading bug

--- LogoWidget.js
+++ LogoWidget.js
@@ -1,18 +1,18 @@
-// define([
-//   "dojo/_base/declare",
-//   "dojo/_base/lang",
-//   "dijit/_WidgetBase",
-//   //"dijit/_TemplatedMixin",
-//   "esri/opsdashboard/WidgetProxy"
-  
-// ], function (declare, lang, _WidgetBase,
-//              _TemplatedMixin, WidgetProxy, templateString) {
-//     return declare("LogoWidget", [
-//       _WidgetBase, 
-//       //_TemplatedMixin, 
-//       WidgetProxy
-//       ], {
+define([
+  "dojo/_base/declare",
+  "dojo/_base/lang",
+  "dijit/_WidgetBase",
+  //"dijit/_TemplatedMixin",
+  "esri/opsdashboard/WidgetProxy" 
+], function (declare, lang, _WidgetBase,
+             //_TemplatedMixin, 
+             WidgetProxy, templateString) {
+    return declare("LogoWidget", [
+      _WidgetBase, 
+      //_TemplatedMixin, 
+      WidgetProxy
+      ], {
 
-//   });
+  });
 
-// });
+});
